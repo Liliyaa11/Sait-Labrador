cebc013898126a96e10bdb5fb8a3b6c6a0921227
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-10-16 13:12:29 +0300

    Add files via upload

---
+++ sait.labrador/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sait.labrador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
